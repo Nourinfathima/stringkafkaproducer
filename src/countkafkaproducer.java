6996afc9fd430ef4bc451e1c1cec148bba9e76a7
nourin <nourin@NTP-LAP-636.chn.nestgroup.net>
2022-11-25 11:25:52 +0530

    firstcommit

---
+++ src/countkafkaproducer.java
@@ -0,0 +1,23 @@
+public class countkafkaproducer {
+    import org.apache.kafka.clients.producer.KafkaProducer;
+    import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+import java.util.Properties;
+    public class Producercount {
+        public static void main(String[] args) {
+            KafkaProducer producer;
+            String broker = "localhost:9092";
+            String topic = "count";
+            Properties props = new Properties();
+            props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, broker);
+            props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+            producer = new KafkaProducer<>(props);
+            String datatosend = "india is my country";
+            producer.send(new ProducerRecord(topic, datatosend));
+            producer.close();
+        }
+    }
+
+}
